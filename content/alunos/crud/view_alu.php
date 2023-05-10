151c2f1617b1ea21b6d0fb4b61bae681465b38aa
PedroCarvalho <105824996+PedroCarvalhoB@users.noreply.github.com>
2023-05-10 19:34:36 -0300

    Add files via upload

---
+++ content/alunos/crud/view_alu.php
@@ -0,0 +1,86 @@
+﻿<?php
+	$id = (int) $_GET['id_aluno'];
+	$sql = mysqli_query($con, "select * from aluno where id_aluno = '".$id."';");
+	$row = mysqli_fetch_array($sql);
+?>
+<div id="main" class="container-fluid">
+	<h3 class="page-header">Visualizar registro do Aluno - <?php echo $id; ?> </h3>
+	<div class="row">
+		<div class="col-md-2">
+			<p><strong>Matrícula</strong></p>
+			<p><?php echo $row['mat_alu'];?></p>
+		</div>
+		<div class="col-md-4">
+			<p><strong>Nome Completo</strong></p>
+			<p><?php echo $row['nome_alu'];?></p>
+		</div>
+		<div class="col-md-2">
+			<p><strong>Data Nascimento</strong></p>
+			<p><?php echo date('d-m-Y',strtotime($row['dt_nasc_alu'])); ?></p>
+		</div>
+		<div class="col-md-2">
+			<p><strong>Escolaridade</strong></p>
+			<p><?php 
+				if($row["esc_alu"]=="M") 
+					echo "Médio"; 
+				else 
+					echo 'Superior'; 
+				?></p>
+		</div>
+		<div class="col-md-2">
+			<p><strong>Sexo</strong></p>
+			<p><?php 
+				if($row["sexo_alu"]=="M") 
+					echo "Masculino"; 
+				else 
+					echo 'Feminino'; 
+				?></p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6">
+			<p><strong>Nome do Pai</strong></p>
+			<p><?php echo $row['pai_alu'];?></p>
+		</div>
+		<div class="col-md-6">
+			<p><strong>Nome da Mãe</strong></p>
+			<p><?php echo $row['mae_alu'];?></p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-4">
+			<p><strong>RG</strong></p>
+			<p><?php echo $row['rg_alu'];?></p>
+		</div>
+		<div class="col-md-4">
+			<p><strong>ORG. EXP.</strong></p>
+			<p><?php echo $row['oe_rg_alu'];?></p>
+		</div>
+		<div class="col-md-4">
+			<p><strong>CPF</strong></p>
+			<p><?php echo $row['cpf_alu'];?></p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-4">
+			<p><strong>CEP</strong></p>
+			<p><?php echo $row['cep_alu'];?></p>
+		</div>
+		<div class="col-md-4">
+			<p><strong>Número</strong></p>
+			<p><?php echo $row['nr_alu'];?></p>
+		</div>
+		<div class="col-md-4">
+			<p><strong>Complemento</strong></p>
+			<p><?php echo $row['comp_alu'];?></p>
+		</div>
+	</div>
+	<hr/>
+	<div id="actions" class="row">
+		<div class="col-md-12">
+			<a href="?page=lista_alu" class="btn btn-default">Voltar</a>
+				<?php echo "<a href=?page=fedita_alu&id_aluno=".$row['id_aluno']." class='btn btn-primary'>Editar</a>";?>
+				<?php echo "<a href=?page=excluir_alu&id_aluno=".$row['id_aluno']." class='btn btn-danger'>Excluir</a>";?>
+		</div>
+	</div>
+</div>
---
+++ content/alunos/sidebar.php
@@ -0,0 +1,66 @@
+<div class="sidebar close">
+    <div class="logo-details">
+        <i class="fa-sharp fa-solid fa-dumbbell fa-rotate-90"></i>
+        <span class="logo_name">Fucking Gym</span>
+    </div>
+    <ul class="nav-links">
+        <li>
+            <a href="?page=home">
+                <i class='bx bxs-home'></i>
+                <span class="link_name">Home</span>
+            </a>
+            <ul class="sub-menu blank">
+                <li><a class="link_name" href="?page=home">Home</a></li>
+            </ul>
+        </li>
+        <li>
+            <div class="iocn-link">
+                <a href="#">
+                    <i class='bx bxs-user' ></i>
+                    <span class="link_name">Dados</span>
+                </a>
+                <i class='bx bxs-chevron-down arrow'></i>
+            </div>
+            <ul class="sub-menu">
+                <li><a class="link_name" href="#">Dados</a></li>
+                <li><a href="?page=lista_alu">Alunos</a></li>
+                <li><a href="?page=lista_usu">Usuários</a></li>
+                <li><a href="?page=lista_prod">Produtos</a></li>
+                <li><a href="?page=lista_disc">Disciplinas</a></li>
+            </ul>
+        </li>
+        <li>
+            <div class="iocn-link">
+                <a href="#">
+                    <i class='bx bx-dumbbell'></i>
+                    <span class="link_name">Treinos</span>
+                </a>
+                <i class='bx bxs-chevron-down arrow'></i>
+            </div>
+            <ul class="sub-menu">
+                <li><a class="link_name" href="#">Treinos</a></li>
+                <li><a href="#">Treino A</a></li>
+                <li><a href="#">Treino B</a></li>
+                <li><a href="#">Treino C</a></li>
+            </ul>
+        </li>
+        <li>
+            <div class="iocn-link">
+                <a href="#">
+                <i class="fa-solid fa-person-walking"></i>
+                    <span class="link_name">Exercícios</span>
+                </a>
+                <i class='bx bxs-chevron-down arrow'></i>
+            </div>
+            <ul class="sub-menu">
+                <li><a class="link_name" href="#">Exercícios</a></li>
+                <li><a href="#">Braços</a></li>
+                <li><a href="#">Costas</a></li>
+                <li><a href="#">Peitoral</a></li>
+                <li><a href="#">Pernas</a></li>
+                <li><a href="#">Abdomen</a></li>
+                <li><a href="#">Ombros</a></li>
+            </ul>
+        </li>
+    </ul>
+</div>
