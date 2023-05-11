1a510516164ce2502f601af5d94e1049238b9a14
leandroferreira30 <96580968+leandroferreira30@users.noreply.github.com>
2023-05-11 17:05:46 -0300

    primeiro commit site comercial

--- index.php
+++ index.php
@@ -1,15 +1,267 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link
+        href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap"
+        rel="stylesheet">
+
+    <title>QuickFit</title>
+    <!--
+
+TemplateMo 548 Training Studio
+
+https://templatemo.com/tm-548-training-studio
+
+-->
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+
+    <link rel="stylesheet" href="assets/css/templatemo-training-studio.css">
+
 </head>
+
 <body>
-    <h1>Página comercial</h1>
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <a href="base/cadastro/fadd_cad.php">Cadastro</a>
-    <a href="base/login.php">Login</a>
+
+    <!-- ***** Preloader Start ***** -->
+    <div id="js-preloader" class="js-preloader">
+        <div class="preloader-inner">
+            <span class="dot"></span>
+            <div class="dots">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    </div>
+    <!-- ***** Preloader End ***** -->
+
+
+    <!-- ***** Header Area Start ***** -->
+    <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+                        <!-- ***** Logo Start ***** -->
+                        <a href="index.html" class="logo">Quick<em> Fit</em></a>
+                        <!-- ***** Logo End ***** -->
+                        <!-- ***** Menu Start ***** -->
+                        <ul class="nav">
+                            <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+                            <li class="scroll-to-section"><a href="#features">Sobre</a></li>
+                            <li class="scroll-to-section"><a href="#our-classes">Funcionamento</a></li>
+                            <li class="main-button"><a href="#">Entrar</a></li>
+                        </ul>
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+                        <!-- ***** Menu End ***** -->
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
+
+    <!-- ***** Main Banner Area Start ***** -->
+    <div class="main-banner" id="top">
+        <video autoplay muted loop id="bg-video">
+            <source src="assets/images/gym-video.mp4" type="video/mp4" />
+        </video>
+
+        <div class="video-overlay header-text">
+            <div class="caption">
+                <h6>Trabalhe duro, fique forte</h6>
+                <h2>fácil com nosso <em>sistema</em></h2>
+                <div class="main-button scroll-to-section">
+                    <a href="#features">torne-se um usuário</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ***** Main Banner Area End ***** -->
+
+    <!-- ***** Features Item Start ***** -->
+    <section class="section" id="features">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 offset-lg-3">
+                    <div class="section-heading">
+                        <h2>Sobre <em>Nós</em></h2>
+                        <img src="assets/images/line-dec.png" alt="waves">
+                    </div>
+                </div>
+                <div>
+                    <ul class="features-items">
+                        <li class="feature-item">
+                            <div class="left-icon">
+                                <img src="assets/images/features-first-icon.png" alt="fourth muscle">
+                            </div>
+                            <div class="left-content">
+                                <h4>Fichas Eletrônicas</h4>
+                                <p>Nosso sistema para academias é a solução perfeita para quem busca otimizar processos e atender melhor seus alunos. Com ele, é possível criar séries de exercícios personalizadas de forma rápida e fácil. Com nosso sistema, você terá acesso a uma vasta biblioteca de exercícios, o que possibilita a criação de treinos personalizados de acordo com as necessidades e objetivos de cada aluno.</p>
+                            </div>
+                        </li>
+                        <li class="feature-item">
+                            <div class="left-icon">
+                                <img src="assets/images/features-first-icon.png" alt="training fifth">
+                            </div>
+                            <div class="left-content">
+                                <h4>Cadastro de Exercícios</h4>
+                                <p>Nosso sistema de cadastro de exercícios para academias foi desenvolvido para tornar o gerenciamento de treinos mais fácil e eficiente. Com ele, é possível cadastrar todos os exercícios utilizados pela academia e organizá-los de forma prática e intuitiva. Cada exercício é cadastrado com suas informações completas, incluindo nome, descrição, músculos trabalhados.</p>
+                            </div>
+                        </li>
+                        <li class="feature-item">
+                            <div class="left-icon">
+                                <img src="assets/images/features-first-icon.png" alt="gym training">
+                            </div>
+                            <div class="left-content">
+                                <h4>Cadastro de Usuários</h4>
+                                <p>Nosso sistema de cadastro permite que academias registrem informações sobre seus alunos e funcionários em um único lugar. Com ele, as academias podem cadastrar informações detalhadas sobre seus funcionários, como nome, cargo, nível de gerenciamento. Também é possível cadastrar informações sobre cada aluno, incluindo nome, idade, altura, peso para a elaboração de um plano de treinamento personalizado. </p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Features Item End ***** -->
+
+    <!-- ***** Call to Action Start ***** -->
+    <section class="section" id="call-to-action">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 offset-lg-1">
+                    <div class="cta-content">
+                        <h2>Don’t <em>think</em>, begin <em>today</em>!</h2>
+                        <p>Ut consectetur, metus sit amet aliquet placerat, enim est ultricies ligula, sit amet dapibus
+                            odio augue eget libero. Morbi tempus mauris a nisi luctus imperdiet.</p>
+                        <div class="main-button scroll-to-section">
+                            <a href="#our-classes">torne-se um usuário</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Call to Action End ***** -->
+
+    <!-- ***** Our Classes Start ***** -->
+    <section class="section" id="our-classes">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 offset-lg-3">
+                    <div class="section-heading">
+                        <h2>Our <em>Classes</em></h2>
+                        <img src="assets/images/line-dec.png" alt="">
+                        <p>Nunc urna sem, laoreet ut metus id, aliquet consequat magna. Sed viverra ipsum dolor,
+                            ultricies fermentum massa consequat eu.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row" id="tabs">
+                <div class="col-lg-4">
+                    <ul>
+                        <li><a href='#tabs-1'><img src="assets/images/tabs-first-icon.png" alt="">First Training
+                                Class</a></li>
+                        <li><a href='#tabs-2'><img src="assets/images/tabs-first-icon.png" alt="">Second Training
+                                Class</a></a></li>
+                        <li><a href='#tabs-3'><img src="assets/images/tabs-first-icon.png" alt="">Third Training
+                                Class</a></a></li>
+                        <li><a href='#tabs-4'><img src="assets/images/tabs-first-icon.png" alt="">Fourth Training
+                                Class</a></a></li>
+                        <div class="main-rounded-button"><a href="#">View All Schedules</a></div>
+                    </ul>
+                </div>
+                <div class="col-lg-8">
+                    <section class='tabs-content'>
+                        <article id='tabs-1'>
+                            <img src="assets/images/training-image-01.jpg" alt="First Class">
+                            <h4>First Training Class</h4>
+                            <p>Phasellus convallis mauris sed elementum vulputate. Donec posuere leo sed dui eleifend
+                                hendrerit. Sed suscipit suscipit erat, sed vehicula ligula. Aliquam ut sem fermentum sem
+                                tincidunt lacinia gravida aliquam nunc. Morbi quis erat imperdiet, molestie nunc ut,
+                                accumsan diam.</p>
+                            <div class="main-button">
+                                <a href="#">View Schedule</a>
+                            </div>
+                        </article>
+                        <article id='tabs-2'>
+                            <img src="assets/images/training-image-02.jpg" alt="Second Training">
+                            <h4>Second Training Class</h4>
+                            <p>Integer dapibus, est vel dapibus mattis, sem mauris luctus leo, ac pulvinar quam tortor a
+                                velit. Praesent ultrices erat ante, in ultricies augue ultricies faucibus. Nam tellus
+                                nibh, ullamcorper at mattis non, rhoncus sed massa. Cras quis pulvinar eros. Orci varius
+                                natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                            <div class="main-button">
+                                <a href="#">View Schedule</a>
+                            </div>
+                        </article>
+                        <article id='tabs-3'>
+                            <img src="assets/images/training-image-03.jpg" alt="Third Class">
+                            <h4>Third Training Class</h4>
+                            <p>Fusce laoreet malesuada rhoncus. Donec ultricies diam tortor, id auctor neque posuere sit
+                                amet. Aliquam pharetra, augue vel cursus porta, nisi tortor vulputate sapien, id
+                                scelerisque felis magna id felis. Proin neque metus, pellentesque pharetra semper vel,
+                                accumsan a neque.</p>
+                            <div class="main-button">
+                                <a href="#">View Schedule</a>
+                            </div>
+                        </article>
+                        <article id='tabs-4'>
+                            <img src="assets/images/training-image-04.jpg" alt="Fourth Training">
+                            <h4>Fourth Training Class</h4>
+                            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
+                                egestas. Aenean ultrices elementum odio ac tempus. Etiam eleifend orci lectus, eget
+                                venenatis ipsum commodo et.</p>
+                            <div class="main-button">
+                                <a href="#">View Schedule</a>
+                            </div>
+                        </article>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Our Classes End ***** -->
+    <hr>
+    <!-- ***** Footer Start ***** -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; 2020 QuickFit</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="assets/js/jquery-2.1.0.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="assets/js/popper.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- Plugins -->
+    <script src="assets/js/scrollreveal.min.js"></script>
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <script src="assets/js/imgfix.min.js"></script>
+    <script src="assets/js/mixitup.js"></script>
+    <script src="assets/js/accordions.js"></script>
+
+    <!-- Global Init -->
+    <script src="assets/js/custom.js"></script>
+
 </body>
+
 </html>
