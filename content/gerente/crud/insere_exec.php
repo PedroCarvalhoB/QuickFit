bd0e1221995acfb8ebff9c381a616920e878e1ce
PedroCarvalho <105824996+PedroCarvalhoB@users.noreply.github.com>
2023-05-16 23:17:53 -0300

    fadd exercícios e view de aluno um pouco bonita

---
+++ content/gerente/crud/insere_exec.php
@@ -0,0 +1,22 @@
+<?php
+
+include '../../../config.php';
+
+$nome             = $_POST["nome_exec"];
+$img              = $_POST["imagem"];
+$descr            = $_POST["desc_exec"];
+
+$sql = "insert into exercicio values ";
+$sql .= "('0','$nome','$img', '$descr');";
+
+// echo $sql; exit;
+
+$resultado = mysqli_query($con, $sql) or die(mysqli_error($erro));
+
+if ($resultado) {
+    header('Location: \QuickFit-main/dashboard.php?page=fadd_exec');
+    mysqli_close($con);
+} else {
+    header('Location: \QuickFit-main/dashboard.php?page=fadd_exec');
+    mysqli_close($con);
+}
