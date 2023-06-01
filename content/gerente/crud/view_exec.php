dbfd7a7e8d1772d08f513575f7256ffb9f53ddd0
Victor H.S. Bahia <115743079+VictorBahia@users.noreply.github.com>
2023-06-01 20:55:59 -0300

    view_exec.php

---
+++ content/gerente/crud/view_exec.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="">
+    <title>Visualizar exercícios</title>
+</head>
+
+<body>
+    <main>
+
+        <div>
+            <p id="flexaoBraco">Flexão de Braço</p>
+            <video autoplay muted loop id="mp4flexaoBraco">
+                <source src="../../assets/mp4/Barra-com-pegada-invertida(360p)" type="video/mp4">
+            </video>
+            <span id="sobreFrexaoBraco">
+                Para fazer uma flexão de braço, você deve seguir os seguintes passos:
+
+                1- Deite-se no chão de barriga para baixo.
+                2 - Apoie as palmas das mãos no solo logo abaixo dos seus ombros.
+                3 - Desloque as mãos um pouco para a lateral, apenas para melhorar a amplitude de movimento.
+                4 - Estenda os cotovelos, subindo o corpo todo de uma vez.
+                5 - Mantenha as costas retas, alinhadas com o tronco.
+
+                A flexão de braços é um exercício que trabalha os músculos do peito, dos ombros e dos braços. Assim, ela
+                tonifica e fortalece músculos importantes como o peitoral maior, o peitoral menor, o tríceps, os
+                deltóides e até mesmo os músculos abdominais e a lombar.
+            </span>
+        </div>
+    </main>
+</body>
+
+</html>
