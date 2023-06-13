11eef8dd883599f53fa166cb1a292820ebfd5ee5
Leandro Ferreira <96580968+leandroferreira30@users.noreply.github.com>
2023-06-13 19:08:23 -0300

    Add files via upload

--- dashboard.php
+++ dashboard.php
@@ -18,6 +18,7 @@ if (!isset($_SESSION))
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
   <link rel="stylesheet" href="assets/css/edit_aluno.css">
+  <link rel="stylesheet" href="assets/css/cadastros.css">
 </head>
 
 <body>
