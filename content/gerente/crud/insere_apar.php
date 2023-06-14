d8044f44285724a932ccaacf3c3e44bea322b2d0
PedroCarvalho <pedrocarvalhobarcellos@gmail.com>
2023-06-14 16:42:45 -0300

    insere_apar e o bd funcionando

--- content/gerente/crud/insere_apar.php
+++ content/gerente/crud/insere_apar.php
@@ -3,21 +3,26 @@ if (!isset($_SESSION))
 session_start();
 
 
-
 $nome             = $_POST["nome_apar"];
 $acad             = $_SESSION['UsuarioAcad'];
 $quant            = $_POST["quant"];
 
-$sql = "insert into aparelho values ";
-$sql .= "('0','$acad','select id_apar from aparelho where nome_aparelho like '$nome'', '$quant');";
+$sql_apar = "select id_apar from aparelho a where nome_apar like '".$nome."';";
+
+$resul_id = mysqli_query($con, $sql_apar);
+
+$id_apar = mysqli_fetch_array($resul_id);
+
+$sql = "insert into apar_acad values ";
+$sql .= "(null,'$acad','".$id_apar['id_apar']."', '$quant');";
 
 
 $resultado = mysqli_query($con, $sql) or die(mysqli_error($erro));
 
 if ($resultado) {
-    header('Location: \QuickFit-main/dashboard.php?page=home');
+    header('Location: \QuickFit/dashboard.php?page=home');
     mysqli_close($con);
 } else {
-    header('Location: \QuickFit-main/dashboard.php?page=fadd_apar');
+    header('Location: \QuickFit/dashboard.php?page=fadd_apar');
     mysqli_close($con);
 }
