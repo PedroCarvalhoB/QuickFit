a768c91644d014c7bbc7c8b8693d2025d50795a7
PedroCarvalho <pedrocarvalhobarcellos@gmail.com>
2023-06-27 15:10:20 -0300

    bloqueando treino antigo sem aviso!

--- content/professor/crud/lista_treino_alu.php
+++ content/professor/crud/lista_treino_alu.php
@@ -10,7 +10,7 @@ $row = mysqli_fetch_array($sql);
         </div>
 
         <div class="col-md-2">
-            <?php echo "<a href=?page=fadd_treino&id=" . $row['id_usu'] . " class='btnsub'>Novo Treino</a>"; ?>
+            <?php echo "<a href=?page=bloquear_treino&id=" . $row['id_usu'] . " class='btnsub'>Novo Treino</a>"; ?>
         </div>
     </div>
 
